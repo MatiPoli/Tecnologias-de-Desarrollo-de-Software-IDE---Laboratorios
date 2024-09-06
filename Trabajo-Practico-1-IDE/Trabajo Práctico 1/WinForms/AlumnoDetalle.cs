8d3319890e679c45a5e3e850fdfb01192b3efbbe
MatiPoli <matiastmarquez@gmail.com>
2024-09-06 13:18:29 -0300

    Añado Trabajo Prácitco 1

---
+++ Trabajo-Practico-1-IDE/Trabajo Práctico 1/WinForms/AlumnoDetalle.cs
@@ -0,0 +1,136 @@
+﻿using Trabajo_Práctico_1.Model;
+using Trabajo_Práctico_1.Services;
+
+
+namespace WindowsForms
+{
+    public partial class AlumnoDetalle : Form
+    {
+        private Alumno alumno;
+        private IEnumerable<Provincia> provincias;
+
+        public Alumno Alumno
+        {
+            get { return alumno; }
+            set
+            {
+                alumno = value;
+                this.SetAlumno();
+            }
+        }
+        public bool EditMode { get; set; } = false;
+
+        public AlumnoDetalle()
+        {
+            InitializeComponent();
+        }
+
+        private void aceptarButton_Click(object sender, EventArgs e)
+        {
+            UniversidadService uniService = new UniversidadService();
+
+            if (this.ValidateAlumno())
+            {
+                this.Alumno.Nombre = nombreTextBox.Text;
+                this.Alumno.Apellido = apellidoTextBox.Text;
+                this.Alumno.Legajo = legajoTextBox.Text;
+                this.Alumno.Direccion = direccionTextBox.Text;
+                this.Alumno.ProvinciaID = this.provincias.ElementAt(provinciaBox.SelectedIndex).Id;
+
+                if (this.EditMode)
+                {
+                    uniService.Update(this.Alumno);
+                }
+                else
+                {
+                    uniService.Add(this.Alumno);
+                }
+
+                this.Close();
+            }
+        }
+
+        private void cancelarButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SetAlumno()
+        {
+            if (this.EditMode)
+            {
+                ProvinciaService provService = new ProvinciaService();
+                this.apellidoTextBox.Text = this.Alumno.Apellido;
+                this.nombreTextBox.Text = this.Alumno.Nombre;
+                this.legajoTextBox.Text = this.Alumno.Legajo;
+                this.direccionTextBox.Text = this.Alumno.Direccion;          
+                this.provinciaBox.Text = provService.Get(this.Alumno.ProvinciaID).Nombre;
+            }
+        }
+
+        private bool ValidateAlumno()
+        {
+            bool isValid = true;
+
+            errorProvider.SetError(nombreTextBox, string.Empty);
+            errorProvider.SetError(apellidoTextBox, string.Empty);
+            errorProvider.SetError(legajoTextBox, string.Empty);
+            errorProvider.SetError(direccionTextBox, string.Empty);
+            errorProvider.SetError(provinciaBox, string.Empty);
+
+            if (this.nombreTextBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(nombreTextBox, "El Nombre es requerido");
+            }
+
+            if (this.apellidoTextBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(apellidoTextBox, "El Apellido es requerido");
+            }
+
+            if (this.legajoTextBox.Text.Length != 5)
+            {
+                isValid = false;
+                errorProvider.SetError(legajoTextBox, "El Legajo tiene un largo incorrecto");
+            }
+
+            if (this.legajoTextBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(legajoTextBox, "El Legajo es requerido");
+            }
+
+            if (this.direccionTextBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(direccionTextBox, "La Direccion es requerida");
+            }
+
+            if (this.provinciaBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(provinciaBox, "La Provincia es requerida");
+            }
+            else if (this.provinciaBox.SelectedIndex == -1)
+            {
+                isValid = false;
+                errorProvider.SetError(provinciaBox, "La Provincia no existe");
+            }
+            return isValid;
+        }
+
+        private void AlumnoDetalle_Load(object sender, EventArgs e)
+        {
+            ProvinciaService provService = new ProvinciaService();
+
+            this.provincias = provService.GetAll();
+            foreach (Provincia provincia in this.provincias)
+            {
+                this.provinciaBox.Items.Add(provincia.Nombre);
+            }
+
+        }
+    }
+}
---
+++ Trabajo-Practico-1-IDE/Trabajo Práctico 1/WinForms/AlumnoLista.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WinForms
+{
+    partial class AlumnoLista
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            eliminarButton = new Button();
+            modificarButton = new Button();
+            agregarButton = new Button();
+            alumnosDataGridView = new DataGridView();
+            provinciasButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)alumnosDataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // eliminarButton
+            // 
+            eliminarButton.Location = new Point(522, 429);
+            eliminarButton.Margin = new Padding(2, 1, 2, 1);
+            eliminarButton.Name = "eliminarButton";
+            eliminarButton.Size = new Size(81, 22);
+            eliminarButton.TabIndex = 3;
+            eliminarButton.Text = "Eliminar";
+            eliminarButton.UseVisualStyleBackColor = true;
+            eliminarButton.Click += eliminarButton_Click;
+            // 
+            // modificarButton
+            // 
+            modificarButton.Location = new Point(607, 429);
+            modificarButton.Margin = new Padding(2, 1, 2, 1);
+            modificarButton.Name = "modificarButton";
+            modificarButton.Size = new Size(81, 22);
+            modificarButton.TabIndex = 4;
+            modificarButton.Text = "Modificar";
+            modificarButton.UseVisualStyleBackColor = true;
+            modificarButton.Click += modificarButton_Click;
+            // 
+            // agregarButton
+            // 
+            agregarButton.Location = new Point(692, 429);
+            agregarButton.Margin = new Padding(2, 1, 2, 1);
+            agregarButton.Name = "agregarButton";
+            agregarButton.Size = new Size(81, 22);
+            agregarButton.TabIndex = 5;
+            agregarButton.Text = "Agregar";
+            agregarButton.UseVisualStyleBackColor = true;
+            agregarButton.Click += agregarButton_Click;
+            // 
+            // alumnosDataGridView
+            // 
+            alumnosDataGridView.AllowUserToOrderColumns = true;
+            alumnosDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            alumnosDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            alumnosDataGridView.Location = new Point(12, 12);
+            alumnosDataGridView.MultiSelect = false;
+            alumnosDataGridView.Name = "alumnosDataGridView";
+            alumnosDataGridView.ReadOnly = true;
+            alumnosDataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            alumnosDataGridView.Size = new Size(760, 413);
+            alumnosDataGridView.TabIndex = 6;
+            alumnosDataGridView.CellContentClick += alumnosDataGridView_CellContentClick;
+            // 
+            // provinciasButton
+            // 
+            provinciasButton.Location = new Point(12, 431);
+            provinciasButton.Name = "provinciasButton";
+            provinciasButton.Size = new Size(115, 23);
+            provinciasButton.TabIndex = 7;
+            provinciasButton.Text = "Listado Provincias";
+            provinciasButton.UseVisualStyleBackColor = true;
+            provinciasButton.Click += provinciasButton_Click;
+            // 
+            // AlumnoLista
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 461);
+            Controls.Add(provinciasButton);
+            Controls.Add(alumnosDataGridView);
+            Controls.Add(agregarButton);
+            Controls.Add(modificarButton);
+            Controls.Add(eliminarButton);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "AlumnoLista";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Universidad";
+            Load += AlumnoLista_Load;
+            ((System.ComponentModel.ISupportInitialize)alumnosDataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button eliminarButton;
+        private Button modificarButton;
+        private Button agregarButton;
+        private DataGridView alumnosDataGridView;
+        private Button provinciasButton;
+    }
+}
